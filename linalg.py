0843e079041706fb97f32386b5d339058e6292ba
FedBeenOne <egorchenkovfedya@gmail.com>
2026-01-20 01:27:06 +0300

    lab

--- linalg.py
+++ linalg.py
@@ -3,21 +3,27 @@ from types import Vector, DenseMatrix
 from typing import Tuple, Optional
 
 def lu_decomposition(A: CSCMatrix) -> Optional[Tuple[CSCMatrix, CSCMatrix]]:
+    """
+    LU-разложение методом Дулиттла.
+    """
     n_rows, n_cols = A.shape
     if n_rows != n_cols:
         return None 
-
+    
     matrix = A.to_dense()
     
     L = [[0.0] * n_rows for _ in range(n_rows)]
     U = [[0.0] * n_rows for _ in range(n_rows)]
 
+    
+
     for i in range(n_rows):
-        
+        # 1. Считаем U (верхнюю треугольную)
         for k in range(i, n_rows):
             sum_upper = sum(L[i][j] * U[j][k] for j in range(i))
             U[i][k] = matrix[i][k] - sum_upper
 
+        # 2. Считаем L (нижнюю треугольную)
         for k in range(i, n_rows):
             if i == k:
                 L[i][i] = 1.0
@@ -32,23 +38,29 @@ def lu_decomposition(A: CSCMatrix) -> Optional[Tuple[CSCMatrix, CSCMatrix]]:
     return CSCMatrix.from_dense(L), CSCMatrix.from_dense(U)
 
 def solve_SLAE_lu(A: CSCMatrix, b: Vector) -> Optional[Vector]:
+    """
+    Решение СЛАУ Ax = b через LU-разложение.
+    """
+    # Проверка размеров
     if A.shape[0] != A.shape[1] or len(b) != A.shape[0]:
         return None
 
-    lu_result = lu_decomposition(A)
-    if lu_result is None:
+    lu_res = lu_decomposition(A)
+    if lu_res is None:
         return None
         
-    L_csc, U_csc = lu_result
+    L_csc, U_csc = lu_res
     L = L_csc.to_dense()
     U = U_csc.to_dense()
     n = len(b)
 
+    # 1. Прямой ход: Ly = b
     y = [0.0] * n
     for i in range(n):
         sum_val = sum(L[i][j] * y[j] for j in range(i))
         y[i] = b[i] - sum_val
 
+    # 2. Обратный ход: Ux = y
     x = [0.0] * n
     for i in range(n - 1, -1, -1):
         sum_val = sum(U[i][j] * x[j] for j in range(i + 1, n))
@@ -61,11 +73,14 @@ def solve_SLAE_lu(A: CSCMatrix, b: Vector) -> Optional[Vector]:
     return x
 
 def find_det_with_lu(A: CSCMatrix) -> Optional[float]:
-    lu_result = lu_decomposition(A)
-    if lu_result is None:
+    """
+    Нахождение определителя.
+    """
+    lu_res = lu_decomposition(A)
+    if lu_res is None:
         return None
         
-    _, U_csc = lu_result
+    _, U_csc = lu_res
     U = U_csc.to_dense()
     
     determinant = 1.0
