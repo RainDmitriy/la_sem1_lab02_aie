ce25f16c7e20b34943ea7c9e8bdac262cc569128
Amir Gergov <505233@niuitmo.ru>
2026-01-27 22:27:03 +0300

    Update CSR.py

--- CSR.py
+++ CSR.py
@@ -17,21 +17,17 @@ class CSRMatrix(Matrix):
         return res
 
     def _add_impl(self, other: 'Matrix') -> 'Matrix':
-        res_dense = self.to_dense()
-        other_dense = other.to_dense()
+        d1, d2 = self.to_dense(), other.to_dense()
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
-                res_dense[i][j] += other_dense[i][j]
-        return CSRMatrix.from_dense(res_dense)
+                d1[i][j] += d2[i][j]
+        return CSRMatrix.from_dense(d1)
 
     def _mul_impl(self, scalar: float) -> 'Matrix':
-        if scalar == 0:
-            return CSRMatrix([], [], [0] * (self.shape[0] + 1), self.shape)
         return CSRMatrix([v * scalar for v in self.data], self.indices, self.indptr, self.shape)
 
     def transpose(self) -> 'Matrix':
         from CSC import CSCMatrix
-        # При транспонировании CSR логически превращается в CSC
         return CSCMatrix(self.data, self.indices, self.indptr, (self.shape[1], self.shape[0]))
 
     def _matmul_impl(self, other: 'Matrix') -> 'Matrix':
@@ -46,9 +42,8 @@ class CSRMatrix(Matrix):
         data, indices, indptr = [], [], [0]
         for i in range(rows):
             for j in range(cols):
-                val = dense_matrix[i][j]
-                if abs(val) > 1e-15:
-                    data.append(val)
+                if dense_matrix[i][j] != 0.0:
+                    data.append(dense_matrix[i][j])
                     indices.append(j)
             indptr.append(len(data))
         return cls(data, indices, indptr, (rows, cols))
@@ -60,4 +55,3 @@ class CSRMatrix(Matrix):
     def _to_csc(self):
         from CSC import CSCMatrix
         return CSCMatrix.from_dense(self.to_dense())
-
