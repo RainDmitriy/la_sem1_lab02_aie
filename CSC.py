f1f2510ccf2a57661215481405394ed8c4522ee6
Amir Gergov <505233@niuitmo.ru>
2026-01-27 22:13:22 +0300

    Update CSC.py

--- CSC.py
+++ CSC.py
@@ -1,6 +1,5 @@
 from base import Matrix
-from type import CSCData, CSCIndices, CSCIndptr, Shape, DenseMatrix
-
+from matrix_types import CSCData, CSCIndices, CSCIndptr, Shape, DenseMatrix
 
 class CSCMatrix(Matrix):
     def __init__(self, data: CSCData, indices: CSCIndices, indptr: CSCIndptr, shape: Shape):
@@ -10,55 +9,50 @@ class CSCMatrix(Matrix):
         self.indptr = list(indptr)
 
     def to_dense(self) -> DenseMatrix:
-        r_count, c_count = self.shape
-        res = [[0.0 for _ in range(c_count)] for _ in range(r_count)]
-        for j in range(c_count):
-            for idx in range(self.indptr[j], self.indptr[j + 1]):
-                row = self.indices[idx]
-                res[row][j] = self.data[idx]
+        rows, cols = self.shape
+        res = [[0.0 for _ in range(cols)] for _ in range(rows)]
+        for j in range(cols):
+            for idx in range(self.indptr[j], self.indptr[j+1]):
+                res[self.indices[idx]][j] = self.data[idx]
         return res
 
     def _add_impl(self, other: 'Matrix') -> 'Matrix':
-        from COO import COOMatrix
-        self_coo = self._to_coo()
-        other_coo = other._to_coo() if hasattr(other, '_to_coo') else COOMatrix.from_dense(other.to_dense())
-        return (self_coo + other_coo)._to_csc()
+        s_dense = self.to_dense()
+        o_dense = other.to_dense()
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                s_dense[i][j] += o_dense[i][j]
+        return CSCMatrix.from_dense(s_dense)
 
     def _mul_impl(self, scalar: float) -> 'Matrix':
-        return CSCMatrix([x * scalar for x in self.data], self.indices, self.indptr, self.shape)
+        return CSCMatrix([v * scalar for v in self.data], self.indices, self.indptr, self.shape)
 
     def transpose(self) -> 'Matrix':
         from CSR import CSRMatrix
-        new_shape = (self.shape[1], self.shape[0])
-        return CSRMatrix(self.data, self.indices, self.indptr, new_shape)
+        return CSRMatrix(self.data, self.indices, self.indptr, (self.shape[1], self.shape[0]))
 
     def _matmul_impl(self, other: 'Matrix') -> 'Matrix':
-        # Перемножаем через плотный формат
-        a_dense = self.to_dense()
-        b_dense = other.to_dense()
-        r_a, c_a = self.shape
-        c_b = other.shape[1]
-
-        res = [[0.0 for _ in range(c_b)] for _ in range(r_a)]
-        for i in range(r_a):
-            for k in range(c_a):
-                if a_dense[i][k] != 0:
-                    for j in range(c_b):
-                        res[i][j] += a_dense[i][k] * b_dense[k][j]
-        return CSCMatrix.from_dense(res)
+        from COO import COOMatrix
+        res_coo = COOMatrix.from_dense(self.to_dense()) @ other
+        return CSCMatrix.from_dense(res_coo.to_dense())
 
     @classmethod
     def from_dense(cls, dense_matrix: DenseMatrix) -> 'CSCMatrix':
+        rows = len(dense_matrix)
+        cols = len(dense_matrix[0]) if rows > 0 else 0
+        data, indices, indptr = [], [], [0]
+        for j in range(cols):
+            for i in range(rows):
+                if dense_matrix[i][j] != 0.0:
+                    data.append(dense_matrix[i][j])
+                    indices.append(i)
+            indptr.append(len(data))
+        return cls(data, indices, indptr, (rows, cols))
+
+    def _to_coo(self) -> 'COOMatrix':
         from COO import COOMatrix
-        return COOMatrix.from_dense(dense_matrix)._to_csc()
+        return COOMatrix.from_dense(self.to_dense())
 
     def _to_csr(self) -> 'CSRMatrix':
-        return self._to_coo()._to_csr()
-
-    def _to_coo(self):
-        from COO import COOMatrix
-        cols = []
-        for j in range(self.shape[1]):
-            for _ in range(self.indptr[j], self.indptr[j + 1]):
-                cols.append(j)
-        return COOMatrix(self.data, self.indices, cols, self.shape)
+        from CSR import CSRMatrix
+        return CSRMatrix.from_dense(self.to_dense())
