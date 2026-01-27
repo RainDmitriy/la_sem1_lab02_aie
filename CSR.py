faa9d5b25d2fa0e80e48a2d00d49aba4c82102b0
Amir Gergov <505233@niuitmo.ru>
2026-01-27 22:23:01 +0300

    Update CSR.py

--- CSR.py
+++ CSR.py
@@ -1,5 +1,5 @@
 from base import Matrix
-from type import CSRData, CSRIndices, CSRIndptr, Shape, DenseMatrix
+from types import CSRData, CSRIndices, CSRIndptr, Shape, DenseMatrix
 
 class CSRMatrix(Matrix):
     def __init__(self, data: CSRData, indices: CSRIndices, indptr: CSRIndptr, shape: Shape):
@@ -17,18 +17,21 @@ class CSRMatrix(Matrix):
         return res
 
     def _add_impl(self, other: 'Matrix') -> 'Matrix':
-        s_dense = self.to_dense()
-        o_dense = other.to_dense()
+        res_dense = self.to_dense()
+        other_dense = other.to_dense()
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
-                s_dense[i][j] += o_dense[i][j]
-        return CSRMatrix.from_dense(s_dense)
+                res_dense[i][j] += other_dense[i][j]
+        return CSRMatrix.from_dense(res_dense)
 
     def _mul_impl(self, scalar: float) -> 'Matrix':
+        if scalar == 0:
+            return CSRMatrix([], [], [0] * (self.shape[0] + 1), self.shape)
         return CSRMatrix([v * scalar for v in self.data], self.indices, self.indptr, self.shape)
 
     def transpose(self) -> 'Matrix':
         from CSC import CSCMatrix
+        # При транспонировании CSR логически превращается в CSC
         return CSCMatrix(self.data, self.indices, self.indptr, (self.shape[1], self.shape[0]))
 
     def _matmul_impl(self, other: 'Matrix') -> 'Matrix':
@@ -43,17 +46,17 @@ class CSRMatrix(Matrix):
         data, indices, indptr = [], [], [0]
         for i in range(rows):
             for j in range(cols):
-                if dense_matrix[i][j] != 0.0:
-                    data.append(dense_matrix[i][j])
+                val = dense_matrix[i][j]
+                if abs(val) > 1e-15:
+                    data.append(val)
                     indices.append(j)
             indptr.append(len(data))
         return cls(data, indices, indptr, (rows, cols))
 
-    def _to_coo(self) -> 'COOMatrix':
+    def _to_coo(self):
         from COO import COOMatrix
         return COOMatrix.from_dense(self.to_dense())
 
-    def _to_csc(self) -> 'CSCMatrix':
+    def _to_csc(self):
         from CSC import CSCMatrix
         return CSCMatrix.from_dense(self.to_dense())
-
