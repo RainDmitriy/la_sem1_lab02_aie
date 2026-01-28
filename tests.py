7cc2c8f856d1b158bb82a39981eb094f71e42ec4
Alicorn757 <131530695+Alicorn757@users.noreply.github.com>
2026-01-29 00:01:46 +0700

    PR test

---
+++ tests.py
@@ -0,0 +1,7 @@
+from COO import *
+
+coom_a = COOMatrix([3, 4, 5], [0, 1, 2], [2, 0, 1], (3, 3))
+coom_b = COOMatrix([2, 3, 4, 7], [0, 1, 2, 1], [2, 0, 1, 1], (3, 3))
+# print(2 * coom_a?\)
+
+print(coom_a + coom_b)
