8c9b64daad7712b8a780530f24ce26a107accb1f
20Nik07 <nikitasw2007@gmail.com>
2026-01-28 17:01:28 +0300

    fix: lab2 linalg.py

--- linalg.py
+++ linalg.py
@@ -15,7 +15,6 @@ def lu_decomposition(A: CSCMatrix) -> Optional[Tuple[CSCMatrix, CSCMatrix]]:
         raise ValueError("Матрица должна быть квадратной")
 
     a = A.to_dense()
-    pivots = list(range(rows))
     l = [[0.0] * rows for _ in range(rows)]
     u = [[0.0] * rows for _ in range(rows)]
 
@@ -25,7 +24,9 @@ def lu_decomposition(A: CSCMatrix) -> Optional[Tuple[CSCMatrix, CSCMatrix]]:
             return None
 
         a[i], a[p_row] = a[p_row], a[i]
-        pivots[i], pivots[p_row] = pivots[p_row], pivots[i]
+        for k in range(i):
+            l[i][k], l[p_row][k] = l[p_row][k], l[i][k]
+
         l[i][i] = 1.0
 
         for j in range(i, rows):
@@ -33,7 +34,7 @@ def lu_decomposition(A: CSCMatrix) -> Optional[Tuple[CSCMatrix, CSCMatrix]]:
         for j in range(i + 1, rows):
             l[j][i] = (a[j][i] - sum(l[j][k] * u[k][i] for k in range(i))) / u[i][i]
 
-    return CSCMatrix.from_dense(l), CSCMatrix.from_dense(u), pivots
+    return CSCMatrix.from_dense(l), CSCMatrix.from_dense(u)
 
 
 def solve_SLAE_lu(A: CSCMatrix, b: Vector) -> Optional[Vector]:
@@ -47,20 +48,18 @@ def solve_SLAE_lu(A: CSCMatrix, b: Vector) -> Optional[Vector]:
     if A.shape[0] != len(b):
         raise ValueError("Размерность A должна совпадать с размером b")
 
-    res = lu_decomposition(A)
-    if res is None:
+    lu = lu_decomposition(A)
+    if lu is None:
         return None
 
-    l, u, pivots = res
+    l, u = lu
     n = len(b)
-    b_perm = [b[pivots[i]] for i in range(n)]
     dense_l = l.to_dense()
     dense_u = u.to_dense()
 
     y = [0.0] * n
     for i in range(n):
-        y[i] = b_perm[i] - sum(dense_l[i][j] * y[j] for j in range(i))
-
+        y[i] = b[i] - sum(dense_l[i][j] * y[j] for j in range(i))
     x = [0.0] * n
     for i in range(n - 1, -1, -1):
         if abs(dense_u[i][i]) < 1e-12:
@@ -79,15 +78,10 @@ def find_det_with_lu(A: CSCMatrix) -> Optional[float]:
     if lu is None:
         return None
 
-    l, u, pivots = lu
+    l, u = lu
     dense_u = u.to_dense()
     det = 1.0
-
     for i in range(len(dense_u)):
         det *= dense_u[i][i]
-
-    swaps = sum(1 for i in range(len(pivots)) if pivots[i] != i)
-    if swaps % 2:
-        det *= -1
 
     return det
