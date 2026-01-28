7a1eedf52381dc11b51c995c8c693582fda0f1b2
20Nik07 <nikitasw2007@gmail.com>
2026-01-28 14:11:25 +0300

    add: LinearAlg Lab2 solutions

--- COO.py
+++ COO.py
@@ -1,45 +1,114 @@
 from base import Matrix
-from types import COOData, COORows, COOCols, Shape, DenseMatrix
+from type import COOData, COORows, COOCols, Shape, DenseMatrix
 
 
 class COOMatrix(Matrix):
     def __init__(self, data: COOData, row: COORows, col: COOCols, shape: Shape):
         super().__init__(shape)
-        pass
+        self.row = row
+        self.col = col
+        self.data = data
 
     def to_dense(self) -> DenseMatrix:
         """Преобразует COO в плотную матрицу."""
-        pass
+        row, col = self.shape
+        matrix = [[0.0 for _ in range(col)] for _ in range(row)]
+        for row, col, data in zip(self.row, self.col, self.data):
+            matrix[row][col] += data
+
+        return matrix
 
     def _add_impl(self, other: 'Matrix') -> 'Matrix':
         """Сложение COO матриц."""
-        pass
+        if not isinstance(other, COOMatrix):
+            print("Складывать можно только COO матрицы")
+            raise TypeError
+
+        rows = self.row + other.row
+        cols = self.col + other.col
+        values = self.data + other.data
+
+        return COOMatrix(values, rows, cols, self.shape)
 
     def _mul_impl(self, scalar: float) -> 'Matrix':
         """Умножение COO на скаляр."""
-        pass
+        values = [i * scalar for i in self.data]
+
+        return COOMatrix(values, self.row, self.col, self.shape)
 
     def transpose(self) -> 'Matrix':
         """Транспонирование COO матрицы."""
-        pass
+        return COOMatrix(self.data,
+                         self.col,
+                         self.row,
+                         (self.shape[1], self.shape[0]))
 
     def _matmul_impl(self, other: 'Matrix') -> 'Matrix':
         """Умножение COO матриц."""
-        pass
+        if self.shape[1] != other.shape[0]:
+            print("Умножение матриц невозможно")
+            raise ValueError
+        csr = self._to_csr()
+        if isinstance(other, COOMatrix):
+            other_csr = other._to_csr()
+            return csr @ other_csr
+        return csr @ other
+
 
     @classmethod
     def from_dense(cls, dense_matrix: DenseMatrix) -> 'COOMatrix':
         """Создание COO из плотной матрицы."""
-        pass
+        rows_num = len(dense_matrix)
+        cols_num = len(dense_matrix[0])
+        rows = []
+        cols = []
+        data = []
+
+        for row in range(rows_num):
+            for col in range(cols_num):
+                value = dense_matrix[row][col]
+                if value != 0:
+                    rows.append(row)
+                    cols.append(col)
+                    data.append(value)
+
+        return COOMatrix(data, rows, cols, (rows_num, cols_num))
 
     def _to_csc(self) -> 'CSCMatrix':
+        from CSC import CSCMatrix
         """
         Преобразование COOMatrix в CSCMatrix.
         """
-        pass
+        sorted_triplets = sorted(zip(self.row, self.col, self.data),
+                                 key=lambda x: (x[1], x[0]))
+        rows, cols, data = zip(*sorted_triplets)
+        indptr = [0] * (self.shape[1] + 1)
+        numbers = [0] * self.shape[1]
+        for c in cols:
+            numbers[c] += 1
+
+        cumsum = 0
+        for i, num in enumerate(numbers):
+            indptr[i] = cumsum
+            cumsum += num
+        indptr[-1] = cumsum
+        return CSCMatrix(data, rows, indptr, self.shape)
 
     def _to_csr(self) -> 'CSRMatrix':
+        from CSR import CSRMatrix
         """
         Преобразование COOMatrix в CSRMatrix.
         """
-        pass
+        sort_coo = sorted(zip(self.row, self.col, self.data))
+        rows, cols, data = zip(*sort_coo)
+        indptr = [0] * (self.shape[0] + 1)
+        numbers = [0] * self.shape[0]
+        for r in rows:
+            numbers[r] += 1
+
+        cum_sum = 0
+        for i, num in enumerate(numbers):
+            indptr[i] = cum_sum
+            cum_sum += num
+        indptr[-1] = cum_sum
+        return CSRMatrix(data, cols, indptr, self.shape)
--- linalg.py
+++ linalg.py
@@ -1,7 +1,8 @@
 from CSC import CSCMatrix
-from CSR import CSRMatrix
-from types import Vector
+# from CSR import CSRMatrix
+from type import Vector
 from typing import Tuple, Optional
+
 
 def lu_decomposition(A: CSCMatrix) -> Optional[Tuple[CSCMatrix, CSCMatrix]]:
     """
@@ -9,18 +10,84 @@ def lu_decomposition(A: CSCMatrix) -> Optional[Tuple[CSCMatrix, CSCMatrix]]:
     Возвращает (L, U) - нижнюю и верхнюю треугольные матрицы.
     Ожидается, что матрица L хранит единицы на главной диагонали.
     """
-    pass
+    rows, cols = A.shape
+    if rows != cols:
+        raise ValueError("Матрица должна быть квадратной")
+
+    dense_a = A.to_dense()
+    u = [[0.0] * rows for _ in range(rows)]
+    l = [[0.0] * rows for _ in range(rows)]
+
+    for i in range(rows):
+        l[i][i] = 1.0
+    for i in range(rows):
+        for m in range(i, rows):
+            summa = 0.0
+            for j in range(i):
+                summa += l[i][j] * u[j][m]
+            u[i][m] = dense_a[i][m] - summa
+        for m in range(i + 1, rows):
+            summa = 0.0
+            for j in range(i):
+                summa += l[m][j] * u[j][i]
+
+            pivot = u[i][i]
+
+            if abs(pivot) < 1e-8:
+                return None
+
+            l[m][i] = (dense_a[m][i]-summa)/pivot
+
+    if abs(u[rows - 1][rows - 1]) < 1e-8:
+        return None
+    csc_l = CSCMatrix.from_dense(l)
+    csc_u = CSCMatrix.from_dense(u)
+
+    return csc_l, csc_u
+
 
 def solve_SLAE_lu(A: CSCMatrix, b: Vector) -> Optional[Vector]:
     """
     Решение СЛАУ Ax = b через LU-разложение.
+    LUx = b
+    Берем Ux = y
+    Решаем Ly = b
+    Решаем Ux = y
     """
-    pass
+    if A.shape[0] != len(b):
+        raise ValueError("Размерность A должна совпадать с размером b")
+
+    l, u = lu_decomposition(A)
+    n = len(b)
+    dense_l = l.to_dense()
+    dense_u = u.to_dense()
+
+    y = [0.0] * n
+    for i in range(n):
+        summa = 0
+        for j in range(i):
+            summa += l[i][j] * y[j]
+        y[i] = b[i] - summa
+
+    x = [0.0] * n
+    for i in range(n - 1, -1, -1):
+        summa = 0
+        for j in range(i + 1, n):
+            summa += u[i][j] * x[j]
+        x[i] = (y[i] - summa) / u[i][i]
+    return x
+
 
 def find_det_with_lu(A: CSCMatrix) -> Optional[float]:
     """
     Нахождение определителя через LU-разложение.
     det(A) = det(L) * det(U)
     """
-    pass
+    det = 1
+    n = A.shape[0]
+    l, u = lu_decomposition(A)
+    dense_u = u.to_dense()
+    for i in range(n):
+        det *= dense_u[i][i]
 
+    return det
