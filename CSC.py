50ac161ece9afab73610334673808a8f34e81988
RuRen <sir.rruren@gmail.com>
2026-02-24 22:11:39 +0300

    fixed (type) problem

--- CSC.py
+++ CSC.py
@@ -1,5 +1,5 @@
 from base import Matrix
-from types import CSCData, CSCIndices, CSCIndptr, Shape, DenseMatrix
+from type import CSCData, CSCIndices, CSCIndptr, Shape, DenseMatrix
 
 
 class CSCMatrix(Matrix):
--- linalg.py
+++ linalg.py
@@ -1,6 +1,6 @@
 from CSC import CSCMatrix
 from CSR import CSRMatrix
-from types import Vector
+from type import Vector
 from typing import Tuple, Optional
 
 def lu_decomposition(A: CSCMatrix) -> Optional[Tuple[CSCMatrix, CSCMatrix]]:
